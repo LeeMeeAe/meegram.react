60c0f2e4d2f5c0023329f79f4dca637902bc400c
meeya <meeya0724@gmail.com>
2021-04-11 18:16:09 +0900

    [Delete] 페이지 삭제

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import DefaultPage from '../pages/DefaultPage';
-import TestPage from '../pages/TestPage';
-import MainPage from '../pages/MainPage';
-import LoginPage from '../pages/LoginPage';
+import React from "react";
+import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
+import ExplorePage from "../pages/ExplorePage";
+import EditProfilePage from "../pages/EditProfilePage";
+import ProfilePage from "../pages/ProfilePage";
 
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 function Routes() {
   return (
     <div>
       <Route path="/" component={MainPage} exact />
-      <Route path="/default" component={DefaultPage} exact/>
-      <Route path="/test" component={TestPage} exact/>
-      <Route path="/login" component={LoginPage} exact/>
+      <Route path="/login" component={LoginPage} exact />
+      <Route path="/explore" component={ExplorePage} exact />
+      <Route path="/editprofile" component={EditProfilePage} exact />
+      <Route path="/profile" component={ProfilePage} exact />
     </div>
   );
 }
